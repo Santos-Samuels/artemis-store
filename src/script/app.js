66b075ce7aa73723251cd991b35c7b074aac9c1e
Santos-Samuels <saamsmith15@gmail.com>
2021-06-02 15:07:46 -0300

    Criação da página Contact

--- src/script/app.js
+++ src/script/app.js
@@ -6,7 +6,7 @@ const loadAccessoryOffer = (products) => {
             html = `
                 <div class="col">
                     <img class="w-100" src="${products[i].image}" alt="${products[i].title}">
-                    <h5>${products[i].title}</h5>
+                    <h5 class="pt-2">${products[i].title}</h5>
                     <p>Por: <span class="fw-bold price fs-2">${products[i].price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</span></p>
                     <a class="btn btn-primary w-100 text-center">COMPRAR</a>
                 </div>
