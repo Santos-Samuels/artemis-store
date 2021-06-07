2f28b9b83e991ef81fb52038c12f43e22b9daae3
Santos-Samuels <saamsmith15@gmail.com>
2021-06-07 15:17:58 -0300

    Mudanças no layout da pagina Admin | mudanças em Vendas e Cadastrar Produto | Criação da página Admin-Login

--- src/pages/admin/script.js
+++ src/pages/admin/script.js
@@ -11,17 +11,23 @@ const setContainerOption = (optionContainer, linkId) => {
     const link = document.querySelector(linkId)
     const containers = document.querySelectorAll('.option-container')
     const optionLinks = document.querySelectorAll('.option-link')
-
+    
     containers.forEach(container => {
         container.classList.add('hide')
     });
-
+    
     optionLinks.forEach(container => {
         container.classList.remove('active')
     });
-
+    
     if(container.classList.contains('hide') == true) {
         link.classList.add('active')
         container.classList.remove('hide')
     }
+}
+
+const toggleSidebar = () => {
+    const sidebarContainer = document.querySelector('#sidebar-container')
+
+    sidebarContainer.classList.contains('toggle-sidebar') ? sidebarContainer.classList.remove('toggle-sidebar') : sidebarContainer.classList.add('toggle-sidebar')
 }
