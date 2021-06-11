c8775b5cc6dd35fea04590c8627ace667f55a7c8
Santos-Samuels <saamsmith15@gmail.com>
2021-06-10 21:32:56 -0300

    Adicionando interação no carrinho | Adiconado interaçõaem  editar produto na página do admin | Outras mudanças

--- src/script/app.js
+++ src/script/app.js
@@ -1,8 +1,8 @@
 const loadAccessoryOffer = (products) => {
-    const accessoryOfferDiv = document.querySelector('#accessory-offer-container')
+    const accessoryOfferContainer = document.querySelector('#accessory-offer-container')
 
     for(i=0; i<products.length;  i++) {
-        if(products[i].category == "calsinha") {
+        if(products[i].type == "calsinha") {
             const html = `
                 <article class="col mb-5">
                     <img class="w-100 rounded" src="${products[i].image}" alt="${products[i].title}">
@@ -12,29 +12,14 @@ const loadAccessoryOffer = (products) => {
                 </article>
             `
 
-            accessoryOfferDiv.insertAdjacentHTML('beforeend', html)
+            accessoryOfferContainer.insertAdjacentHTML('beforeend', html)
         }
     }
 }
 
-
-const ToggleModal = (modalContainer) => {
-    const modal = document.querySelector(modalContainer)
-    const body = document.querySelector('body')
-
-    if(modal.classList.contains('hide') == true) {
-        modal.classList.remove('hide')
-        body.style.overflow = "hidden"
-    }
-    else {
-        modal.classList.add('hide')
-        body.style.overflow = "auto"
-    }
-}
-
-
 const loadProducts = (products) => {
-    const productsDiv = document.querySelector('#products-container')
+    const productsContainer = document.querySelector('#products-container')
+    
 
     products.forEach(product => {
         const html = `
@@ -42,14 +27,101 @@ const loadProducts = (products) => {
                 <img class="rounded" src="${product.image}" alt="${product.title}">
                 <h5 class="pt-2">${product.title}</h5>
                 <p>Por: <span class="fw-bold price fs-2">${product.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</span></p>
-                <a class="btn btn-primary w-100 text-center">COMPRAR</a>
+                <a class="btn btn-primary w-100 text-center" onclick="addProductBag(bag, ${product.id}, '${product.title}', ${product.price}, '${product.image}', ${product.stock}, ${1})" data-bs-toggle="offcanvas" data-bs-target="#userBag" aria-controls="userBag">COMPRAR</a>
             </article>
         `
 
-        productsDiv.insertAdjacentHTML('beforeend', html)
+        productsContainer.insertAdjacentHTML('beforeend', html)
     });
 }
 
-// const loadSales = () => {
 
-// }
+const loadBag = (bag) => {
+    const bagContainer = document.querySelector('#bag-container-products')
+    const bagFooterContainer = document.querySelector('#bag-container-footer')
+
+    bagContainer.querySelectorAll('article').forEach(item => {
+        item.remove()
+    })
+
+    bagFooterContainer.querySelectorAll('payment').forEach(content => {
+        content.remove()
+    })
+
+    
+
+    if(bag.length == 0) {
+        const html = `
+            <article class="text-center mt-5 text-secondary">
+                <i class="bi bi-info-circle fs-1"></i>
+                <h3 class="">Seu carrinho está vazio</h3>
+            </article>
+        `
+        bagContainer.insertAdjacentHTML('beforeend', html)
+    }
+    else {
+        let bagTotalPrice = 0
+
+        bag.forEach(item => {
+            const html = `
+                <article class="row g-2 mb-3 me-1">
+                    <div class="col-3 me-2">
+                        <img class="bag-product-image rounded me-3" src="${item.image}" alt="">
+                    </div>
+                    <div class="me-4 col">
+                        <h6>${item.title}</h6>
+                        <input class="bag-product-quantity form-control" type="number" name="bag-product-quantity" id="" min="0" max="${item.stock}" value="${item.quantity}">
+                    </div>
+                    <div class="col-2">
+                        <p class="mb-0 bag-product-price">${item.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+                        <span class="primary-text primary-text-hover cursor-pointer bag-trash-button" id="${item.id}" onclick="removeProductBag(bag, ${item.id})"><i class="bi bi-trash-fill fs-4"></i></span>
+                    </div>
+                </article>
+            `
+            bagTotalPrice += item.price
+            bagContainer.insertAdjacentHTML('beforeend', html)
+        });
+        
+        bagFooterContainer.innerHTML = `
+            <payment class="row pt-3">
+                <h5 class="col text-secondary">Total</h5>
+                <h5 class="col text-end fw-bold">${bagTotalPrice.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</h5>
+            </payment>
+            <payment class="mt-4">
+                <button class="btn btn-primary w-100" id="bag-button-finalizar-compra">Finalizar Compra</button>
+            </payment>
+        `
+    }
+}
+
+
+const addProductBag = (bag, productId, productTitle, productPrice, productImage, productStock, productQuantity) => {
+    let valid = bag.find(item => { return item.id === productId })
+
+    if(valid != undefined && productQuantity < productStock) {
+        bag.forEach(item => {
+            if(item.id == productId) {
+                item.quantity += 1
+            }
+        });
+    }
+    else bag.push({'id': productId, 'title': productTitle, 'price': productPrice, 'image': productImage, 'stock': productStock, 'quantity': productQuantity})
+    
+    loadBag(bag)
+}
+
+const removeProductBag = (bag, productId) => {
+    if(bag.length != 0) {
+        bag.forEach((item, index) => {
+            if(item.id == productId) {
+                bag.splice(index, 1)
+            }
+         })
+    }
+
+    loadBag(bag)
+}
+
+
+const searchProduct = (products, searchTerm) => products.filter(product => product.title.includes(`${searchTerm}`))
+
--- src/script/products-list.js
+++ src/script/products-list.js
@@ -1,57 +1,185 @@
 var products = [
     {
+        id: 1,
         title: "Colar blue stone",
-        category: "colar",
+        type: "colar",
+        category: "semijóia",
         price: 29.90,
-        image: "/assets/img/colar-blue-stone.jpeg"
+        promo: 24.90,
+        image: "/assets/img/colar-blue-stone.jpeg",
+        description: `É que eu era pequeninin' tipo Plutão
+        Você veio do nada, yaya, me deu a mão
+        Depois me fez voar por onde eu jamais voei
+        E com o passar dos anos olha o que a gente fez, yeah
+        Gata, eu fiz meu corre, né?
+        Em silêncio é sério, eu nem sequer postei story, né?
+        Hoje eu boto qualquer Jordan que eu quiser no meu pé
+        Deixo eles na bota só falando
+        Abaixo do rastreador ninguém sabe onde eu tô
+        Eu sigo no meu plano`,
+        stock: 5,
+        colorList: "branco, preto, prata, ouro",
+        sizeList: "25, 30, 35, 45"
     },
 
     {
+        id: 2,
         title: "Alianças simple style",
-        category: "aliança",
+        type: "aliança",
+        category: "semijóia",
         price: 69.90,
-        image: "/assets/img/aliancas-white-stone.jpeg"
+        promo: 64.90,
+        image: "/assets/img/aliancas-white-stone.jpeg",
+        description: `É que eu era pequeninin' tipo Plutão
+        Você veio do nada, yaya, me deu a mão
+        Depois me fez voar por onde eu jamais voei
+        E com o passar dos anos olha o que a gente fez, yeah
+        Gata, eu fiz meu corre, né?
+        Em silêncio é sério, eu nem sequer postei story, né?
+        Hoje eu boto qualquer Jordan que eu quiser no meu pé
+        Deixo eles na bota só falando
+        Abaixo do rastreador ninguém sabe onde eu tô
+        Eu sigo no meu plano`,
+        stock: 12,
+        colorList: "branco, preto, prata, ouro",
+        sizeList: "25, 30, 35, 45"
     },
 
     {
+        id: 3,
         title: "Colar white stone",
-        category: "colar",
+        type: "colar",
+        category: "semijóia",
         price: 29.90,
-        image: "/assets/img/colar-white-stone.jpeg"
+        promo: 24.90,
+        image: "/assets/img/colar-white-stone.jpeg",
+        description: `É que eu era pequeninin' tipo Plutão
+        Você veio do nada, yaya, me deu a mão
+        Depois me fez voar por onde eu jamais voei
+        E com o passar dos anos olha o que a gente fez, yeah
+        Gata, eu fiz meu corre, né?
+        Em silêncio é sério, eu nem sequer postei story, né?
+        Hoje eu boto qualquer Jordan que eu quiser no meu pé
+        Deixo eles na bota só falando
+        Abaixo do rastreador ninguém sabe onde eu tô
+        Eu sigo no meu plano`,
+        stock: 9,
+        colorList: "branco, preto, prata, ouro",
+        sizeList: "25, 30, 35, 45"
     },
 
     {
+        id: 4,
         title: "Conjunto joia gold",
-        category: "conjunto",
+        type: "conjunto",
+        category: "semijóia",
         price: 119.90,
-        image: "/assets/img/conjunto-joias-gold.jpeg"
+        promo: 114.90,
+        image: "/assets/img/conjunto-joias-gold.jpeg",
+        description: `É que eu era pequeninin' tipo Plutão
+        Você veio do nada, yaya, me deu a mão
+        Depois me fez voar por onde eu jamais voei
+        E com o passar dos anos olha o que a gente fez, yeah
+        Gata, eu fiz meu corre, né?
+        Em silêncio é sério, eu nem sequer postei story, né?
+        Hoje eu boto qualquer Jordan que eu quiser no meu pé
+        Deixo eles na bota só falando
+        Abaixo do rastreador ninguém sabe onde eu tô
+        Eu sigo no meu plano`,
+        stock: 1,
+        colorList: "branco, preto, prata, ouro",
+        sizeList: "25, 30, 35, 45"
     },
 
     {
+        id: 5,
         title: "Calsinha cintura alta nude",
-        category: "calsinha",
+        type: "calsinha",
+        category: "semijóia",
         price: 29.90,
-        image: "https://mash.vteximg.com.br/arquivos/ids/180722/CALCINHA-CINTURA-ALTA-SEMI-COMPRESSAO-MICROFIBRA-SEM-COSTURA-SHE_76101.jpg?v=637357050169270000"
+        promo: 24.90,
+        image: "https://mash.vteximg.com.br/arquivos/ids/180722/CALCINHA-CINTURA-ALTA-SEMI-COMPRESSAO-MICROFIBRA-SEM-COSTURA-SHE_76101.jpg?v=637357050169270000",
+        description: `É que eu era pequeninin' tipo Plutão
+        Você veio do nada, yaya, me deu a mão
+        Depois me fez voar por onde eu jamais voei
+        E com o passar dos anos olha o que a gente fez, yeah
+        Gata, eu fiz meu corre, né?
+        Em silêncio é sério, eu nem sequer postei story, né?
+        Hoje eu boto qualquer Jordan que eu quiser no meu pé
+        Deixo eles na bota só falando
+        Abaixo do rastreador ninguém sabe onde eu tô
+        Eu sigo no meu plano`,
+        stock: 2,
+        colorList: "branco, preto, prata, ouro",
+        sizeList: "25, 30, 35, 45"
     },
 
     {
+        id: 6,
         title: "Calsinha cintura alta preto",
-        category: "calsinha",
+        type: "calsinha",
+        category: "semijóia",
         price: 24.99,
-        image: "https://mash.vteximg.com.br/arquivos/ids/180722/CALCINHA-CINTURA-ALTA-SEMI-COMPRESSAO-MICROFIBRA-SEM-COSTURA-SHE_76101.jpg?v=637357050169270000"
+        promo: 24.90,
+        image: "https://mash.vteximg.com.br/arquivos/ids/180722/CALCINHA-CINTURA-ALTA-SEMI-COMPRESSAO-MICROFIBRA-SEM-COSTURA-SHE_76101.jpg?v=637357050169270000",
+        description: `É que eu era pequeninin' tipo Plutão
+        Você veio do nada, yaya, me deu a mão
+        Depois me fez voar por onde eu jamais voei
+        E com o passar dos anos olha o que a gente fez, yeah
+        Gata, eu fiz meu corre, né?
+        Em silêncio é sério, eu nem sequer postei story, né?
+        Hoje eu boto qualquer Jordan que eu quiser no meu pé
+        Deixo eles na bota só falando
+        Abaixo do rastreador ninguém sabe onde eu tô
+        Eu sigo no meu plano`,
+        stock: 8,
+        colorList: "branco, preto, prata, ouro",
+        sizeList: "25, 30, 35, 45"
     },
 
     {
+        id: 7,
         title: "Calsinha cintura alta nude",
-        category: "calsinha",
+        type: "calsinha",
+        category: "semijóia",
         price: 29.90,
-        image: "https://mash.vteximg.com.br/arquivos/ids/180722/CALCINHA-CINTURA-ALTA-SEMI-COMPRESSAO-MICROFIBRA-SEM-COSTURA-SHE_76101.jpg?v=637357050169270000"
+        promo: 24.90,
+        image: "https://mash.vteximg.com.br/arquivos/ids/180722/CALCINHA-CINTURA-ALTA-SEMI-COMPRESSAO-MICROFIBRA-SEM-COSTURA-SHE_76101.jpg?v=637357050169270000",
+        description: `É que eu era pequeninin' tipo Plutão
+        Você veio do nada, yaya, me deu a mão
+        Depois me fez voar por onde eu jamais voei
+        E com o passar dos anos olha o que a gente fez, yeah
+        Gata, eu fiz meu corre, né?
+        Em silêncio é sério, eu nem sequer postei story, né?
+        Hoje eu boto qualquer Jordan que eu quiser no meu pé
+        Deixo eles na bota só falando
+        Abaixo do rastreador ninguém sabe onde eu tô
+        Eu sigo no meu plano`,
+        stock: 7,
+        colorList: "branco, preto, prata, ouro",
+        sizeList: "25, 30, 35, 45"
     },
 
     {
+        id: 8,
         title: "Calsinha cintura alta preto",
-        category: "calsinha",
+        type: "calsinha",
+        category: "semijóia",
         price: 24.99,
-        image: "https://mash.vteximg.com.br/arquivos/ids/180722/CALCINHA-CINTURA-ALTA-SEMI-COMPRESSAO-MICROFIBRA-SEM-COSTURA-SHE_76101.jpg?v=637357050169270000"
+        promo: 19.90,
+        image: "https://mash.vteximg.com.br/arquivos/ids/180722/CALCINHA-CINTURA-ALTA-SEMI-COMPRESSAO-MICROFIBRA-SEM-COSTURA-SHE_76101.jpg?v=637357050169270000",
+        description: `É que eu era pequeninin' tipo Plutão
+        Você veio do nada, yaya, me deu a mão
+        Depois me fez voar por onde eu jamais voei
+        E com o passar dos anos olha o que a gente fez, yeah
+        Gata, eu fiz meu corre, né?
+        Em silêncio é sério, eu nem sequer postei story, né?
+        Hoje eu boto qualquer Jordan que eu quiser no meu pé
+        Deixo eles na bota só falando
+        Abaixo do rastreador ninguém sabe onde eu tô
+        Eu sigo no meu plano`,
+        stock: 6,
+        colorList: "branco, preto, prata, ouro",
+        sizeList: "25, 30, 35, 45"
     },
 ]
